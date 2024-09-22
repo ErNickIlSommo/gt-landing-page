2f3e7e420c47ced9c958c463d722beb86b366b6f
Nicola <44905646+ErNickIlSommo@users.noreply.github.com>
2024-09-22 18:59:42 +0200

    add structure for logo in mobile website

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -4,28 +4,27 @@ import { HashLink as Link } from "react-router-hash-link";
 
 const SiteNavBar = () => {
   return (
-    <div className="md:flex justify-between hidden md:visible">
-      <div>
-        <div className="uppercase">
-          <Link smooth className="" to="/#">
-            <p>gym track</p>
+    <div>
+      <div className="md:flex justify-between hidden md:visible">
+        <div>
+          <div className="uppercase">
+            <Link smooth className="" to="/#">
+              <p>gym track</p>
+            </Link>
+          </div>
+        </div>
+        <div className="flex gap-10 uppercase">
+          <Link smooth className="" to="/#what-is-gymtrack">
+            Who we are
+          </Link>
+          <Link smooth className="" to="/#family">
+            Join us
           </Link>
         </div>
       </div>
-      <div className="flex gap-10 uppercase">
-        <Link smooth className="" to="/#what-is-gymtrack">
-          Who we are
-        </Link>
-        <Link smooth className="" to="/#family">
-          Join us
-        </Link>
-        {/* <Link smooth className="" to="/#work">
-          Work with us
-        </Link> */}
-        {/* <Link smooth className="" to="/#contact">
-          Contacts
-        </Link> */}
-      </div>
+      {/* <div className="block md:hidden w-full items-center align-middle">
+        <p className="text-center text-5xl font-semibold">Gym Track</p>
+      </div> */}
     </div>
   );
 };
